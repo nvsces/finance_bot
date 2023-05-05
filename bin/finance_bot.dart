e282020d8c0290555bda3482053cf2adabcb645f
nvsces <nvsces@yandex.ru>
2023-05-05 20:29:47 +0300

    initial commit

---
+++ bin/finance_bot.dart
@@ -0,0 +1,5 @@
+import 'package:finance_bot/finance_bot.dart' as finance_bot;
+
+void main(List<String> arguments) {
+  print('Hello world: ${finance_bot.calculate()}!');
+}
---
+++ test/finance_bot_test.dart
@@ -0,0 +1,8 @@
+import 'package:finance_bot/finance_bot.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
